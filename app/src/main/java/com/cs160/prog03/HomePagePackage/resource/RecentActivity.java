fd49946c42612232e13dcfe0d075ed377ebf1b0f
Saul <saul@Windows-10.local>
2022-03-08 15:52:03 -0800

    Modifications to the homepage fragment

---
+++ app/src/main/java/com/cs160/prog03/HomePagePackage/resource/RecentActivity.java
@@ -0,0 +1,11 @@
+package com.cs160.prog03.HomePagePackage.resource;
+
+public class RecentActivity {
+    private String activity;
+    private String time;
+
+    public RecentActivity(String activity, String time) {
+        this.activity = activity;
+        this.time = time;
+    }
+}
